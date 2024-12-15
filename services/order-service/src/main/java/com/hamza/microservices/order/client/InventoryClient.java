a3e01359fdc9f06a476b3fb89d5773753fb45f4c
ChazzitUP <hamzarezgui.education@gmail.com>
2024-12-15 22:55:46 +0100

    configuring rest client for the inventory service

--- services/order-service/src/main/java/com/hamza/microservices/order/client/InventoryClient.java
+++ services/order-service/src/main/java/com/hamza/microservices/order/client/InventoryClient.java
@@ -1,23 +1,14 @@
 package com.hamza.microservices.order.client;
 
-import com.hamza.microservices.order.inventory.InventoryRequest;
-import com.hamza.microservices.order.inventory.InventoryResponse;
-import jakarta.validation.Valid;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-@FeignClient(
-        name = "inventory-service",
-        url = "http://localhost:8070"
-)
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.PostExchange;
+
 public interface InventoryClient {
-    @GetMapping("/api/v1/inventory")
+    @GetExchange("/api/v1/inventory")
     boolean IsInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
-    @PostMapping("/api/v1/inventory")
-    ResponseEntity<InventoryResponse> createInventory(@RequestBody @Valid InventoryRequest inventoryRequest);
-
-    @PostMapping("/api/v1/inventory/deduct")
+    @PostExchange("/api/v1/inventory/deduct")
     void deductInventory(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
